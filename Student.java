b63a1864c9396af9a4e8525d4e759ec3c2367cff
vaishali <mca8@kletech.ac.in>
2024-05-17 16:32:08 +0530

    maintwo files

---
+++ Student.java
@@ -0,0 +1,22 @@
+public class  Student
+{
+    public  static void main (string []Args)
+    {
+    string[] course  = "BCA";
+    string []course  = "MCA";
+    int[]  marks1 = 
+    int []  marks2 = " ";
+     
+        System out  println ("course 1 "+ course[i] );
+        System  out println ("course 2 "+ course[i] );
+
+   for (int i=1; i<=total marks ; i++);
+   {
+        System out println("marks1"+ marks[i]);
+        System out println("marks2"+ marks[i]);
+             
+     
+    
+
+    
+}
