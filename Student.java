f0a8cc832bba6470a8a387f38782ca9babf27f3e
mca8@kleteh.ac.in <mca8@kletech.ac.in>
2024-05-17 16:55:26 +0530

    main

--- Student.java
+++ Student.java
@@ -1,21 +1,49 @@
 public class  Student
 {
-    public  static void main (string []Args)
+    public Static void main(string [args]);
     {
-    string[] course  = "BCA";
-    string []course  = "MCA";
-    int[]  marks1 = 
-    int []  marks2 = " ";
-     
-        System out  println ("course 1 "+ course[i] );
-        System  out println ("course 2 "+ course[i] );
+        int[i] course1 marks = "75";
+        int[i] course2 marks = "75";
+        int[i] course3  marks = "75";
+        total marks = course 1 marks +course 2 marks + course 3 marks
+        double marks = total marks\3;
 
-   for (int i=1; i<=total marks ; i++);
-   {
-        System out println("marks1"+ marks[i]);
-        System out println("marks2"+ marks[i]);
-             
-     
+        for (int i =1; i<=total marks; i++);
+        { 
+
+        char grade 
+
+        
+        if (marks >=90 )
+        {
+            grade = 'S'
+        }
+        else if (marks >= 80 && marks <=90)
+        {
+            grade = 'A'
+        }
+        else if (marks >= 65 && marks <=80)
+        {
+            grade = 'B'
+        }
+        else if (marks >= 50 && marks <=80)
+        {
+            grade = 'C'
+        }
+        else if (marks >= 40 && marks <=50)
+        {
+            grade = 'D'
+        }
+        else {
+            grade = 'F'
+        }
+    }
+}
+}
+
+        
+
+    }
     
 
     
